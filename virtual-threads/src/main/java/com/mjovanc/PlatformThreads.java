1659558513719b148631fe16e4e0582286eb60e3
mjovanc <mjovanc@icloud.com>
2023-11-03 18:35:37 +0100

    Initializing project

---
+++ virtual-threads/src/main/java/com/mjovanc/PlatformThreads.java
@@ -0,0 +1,7 @@
+package com.mjovanc;
+
+public class PlatformThreads {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ virtual-threads/src/main/java/com/mjovanc/VirtualThreads.java
@@ -0,0 +1,7 @@
+package com.mjovanc;
+
+public class VirtualThreads {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
